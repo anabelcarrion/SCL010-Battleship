35db150d4860bccfc0c03ac29397dde546324826
Estefanía Romero-Cors <zotapianola@gmail.com>
2019-12-02 19:09:25 -0300

    limpieza general de código y algunas cosas adicionales como id en divs

--- game/src/Components/GameBoard.js
+++ game/src/Components/GameBoard.js
@@ -1,4 +1,4 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 import { usePieceState } from './Pieces';
 
 import './GameBoard.css';
@@ -10,51 +10,64 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 
-export default  function GameBoard () { 
-        //declarando tamaño de la tabla
-        let tableHeight=5;
-        let tableWidth=5;
-        let table=[];
-        
-        //creando la tabla
-        for (let h=0; h<tableHeight; h++){
-            let row =[];
-            for(let w=0; w<tableWidth; w++){
-                row[w]={state:"false",x:w,y:h};
-            }
-            table[h]=row;
-        }
-        
-        //estado
-        const [tableState, setTableState]=useState(table);
+// tablero
+const GameBoard = () => {
+  // fijar tamaño de tabla
+  let tableHeight = 10;
+  let tableWidth = 10;
+  let table = [];
 
-        //marcar una celda de la tabla y cambiarla de estadp
-        function setPiece(x,y){
-            console.log(x,y)
-            let newTable = tableState.map(x=>x);
-            newTable[x][y].state = "true";
-            console.log(newTable)
-            return newTable;
-        }
-    
-        return (
-            <div id="gameBoard">
-              <div id="boardPlayer1">  
-                <Paper >
-                    <Table id="boardPlayer1">
-                        <TableBody>
-                            {tableState.map(row => (
-                                <TableRow>
-                                    {row.map(position => ( 
-                                        <TableCell className="square" onClick={()=>setTableState(setPiece(position.x,position.y))}>
-                                            {tableState[position.x][position.y].state}</TableCell>
-                                    ))}
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </Paper>
-              </div>
-            </div> 
-        )
-       }
+  // crear la tabla
+  for (let h = 0; h < tableHeight; h++) {
+    let row = [];
+    for (let w = 0; w < tableWidth; w++) {
+      row[w] = { state: 'false', x: w, y: h };
+    }
+    table[h] = row;
+  }
+
+  // estado
+  const [tableState, setTableState] = useState(table);
+
+  // marcar una celda de la tabla y cambiarla de estado
+  const setPiece = (x, y) => {
+    let newTable = tableState.map(x => x);
+    newTable[x][y].state = 'true';
+    console.log(tableState);
+    return newTable;
+  };
+
+  const setID = position => {
+    return parseInt(position.x.toString() + position.y.toString()) + 1;
+  };
+  return (
+    <div id='gameBoard'>
+      <div id='boardPlayer1'>
+        <Paper>
+          <Table id='boardPlayer1'>
+            <TableBody>
+              {tableState.map(row => (
+                <TableRow>
+                  {row.map(position => (
+                    <TableCell
+                      id={setID(position)}
+                      data={[position.x, position.y]}
+                      className='square'
+                      onClick={() =>
+                        setTableState(setPiece(position.x, position.y))
+                      }
+                    >
+                      {tableState[position.x][position.y].state}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
+    </div>
+  );
+};
+
+export default GameBoard;
--- game/src/Components/Pieces.js
+++ game/src/Components/Pieces.js
@@ -1,70 +1,99 @@
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './GameBoard.css';
-import { func } from 'prop-types';
+// import { func } from 'prop-types';
 
-//creando los objeto con los valores que tendran las piezas del juego(esta data luego se guardara en firebase)  
-let guidePiece={
-    name:"",
-    size: "",
-    orientation:""  
-}
-let piece1={
-    name:"perro1",
-    size: "unCuadrado",
-    orientation:"vertical"
-}
-let  piece2={
-    name:"perro2",
-    size: "dosCuadrado",
-    orientation:"vertical"
-}
-let  piece3={
-    name:"perro3",
-    size: "tresCuadrado",
-    orientation:"vertical"
-}
-let  piece4={
-    name:"perro4",
-    size: "seisCuadrado",
-    orientation:"vertical"
-}
-    
-//hooks
-export function usePieceState(){
+// crear los objetos con los valores que tendrán las piezas del juego (esta data luego se guardara en firebase)
+let guidePiece = {
+  name: '',
+  size: '',
+  orientation: ''
+};
+let piece1 = {
+  name: 'perro1',
+  size: '[1,4]',
+  orientation: 'vertical'
+};
+let piece2 = {
+  name: 'perro2',
+  size: '[1,2]',
+  orientation: 'vertical'
+};
+let piece3 = {
+  name: 'perro3',
+  size: 'tresCuadrado',
+  orientation: 'vertical'
+};
+let piece4 = {
+  name: 'perro4',
+  size: 'seisCuadrado',
+  orientation: 'vertical'
+};
 
-    const [selectedPiece, setSelectedPiece]=useState({
-        name:"",
-        size: "",
-        orientation:""
+// hooks
+export const usePieceState = () => {
+  const [selectedPiece, setSelectedPiece] = useState({
+    name: '',
+    size: '',
+    orientation: ''
+  });
+  useEffect(() => {
+    setSelectedPiece({
+      guidePiece
     });
-    useEffect(()=>{
-        setSelectedPiece({
-            guidePiece      
-        })
-        console.log(guidePiece);
-    },[])
-    return selectedPiece;
-}
+    console.log(guidePiece);
+  }, []);
+  return selectedPiece;
+};
 
-//funcion que asigna los valores de las piezas
+// asignar valores a las piezas
 
-function selectPiece (piece){
-    guidePiece=piece;
- console.log(guidePiece)
-}
+const selectPiece = piece => {
+  guidePiece = piece;
+  console.log(guidePiece);
+};
 
-export default  function Pieces (){ 
-    const[pieceState,setPieceState] = useState(guidePiece);
+const Pieces = () => {
+  const [pieceState, setPieceState] = useState(guidePiece);
 
-    return (
-        <div>
-            <p>esta seleccionado la pieza: {pieceState.name}</p>
-            <p>orientacion:{pieceState.orientation}</p>
-            <p>tamaño:{pieceState.size}</p>
-            <button onClick={()=>{selectPiece(piece1);setPieceState(piece1)}}>perro1</button> 
-            <button onClick={()=>{selectPiece(piece2);setPieceState(piece2)}}>perro2</button>
-            <button onClick={()=>{selectPiece(piece3);setPieceState(piece3)}}>perro3</button>
-            <button onClick={()=>{selectPiece(piece4);setPieceState(piece4)}}>perro4</button>
-        </div>
-    )    
-}
+  return (
+    <div>
+      <p>esta seleccionado la pieza: {pieceState.name}</p>
+      <p>orientacion:{pieceState.orientation}</p>
+      <p>tamaño:{pieceState.size}</p>
+      <button
+        onClick={() => {
+          selectPiece(piece1);
+          setPieceState(piece1);
+        }}
+      >
+        perro1
+      </button>
+      <button
+        onClick={() => {
+          selectPiece(piece2);
+          setPieceState(piece2);
+        }}
+      >
+        perro2
+      </button>
+      <button
+        onClick={() => {
+          selectPiece(piece3);
+          setPieceState(piece3);
+        }}
+      >
+        perro3
+      </button>
+      <button
+        onClick={() => {
+          selectPiece(piece4);
+          setPieceState(piece4);
+        }}
+      >
+        perro4
+      </button>
+    </div>
+  );
+};
+
+export default Pieces;
