63746db37773f1e3800f47413f32cbb38c7fb0c0
Zotapianola <zotapianola@gmail.com>
2019-12-05 20:51:54 -0300

    Revert "creando ruta para cambiar de vistas en la app"

---
+++ game/src/Components/GameRoom.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import GameBoard1 from './GameBoard1';
+import GameBoard2 from './GameBoard2';
+
+
+function GameRooms() {
+  return (
+    <div>
+     <h1>Estamos en la sala de juego</h1>
+    <GameBoard1/>
+    <GameBoard2/>
+    </div>
+  );
+}
+
+export default GameRooms;
