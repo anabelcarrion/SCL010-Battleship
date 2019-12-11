3d0f81a75d9b55d36b0464ef9b2d52aefbf05464
anabelcarrion <anabelcarrion3110@gmail.com>
2019-12-10 21:03:40 -0300

    las piezas se puede cambiar de posicion en el tablero

--- game/src/Components/Pieces.js
+++ game/src/Components/Pieces.js
@@ -2,126 +2,52 @@ import React, { Component, useEffect, useState, useContext } from 'react';
 import './GameBoard.css';
 import {GameBoardPaintContext} from './GameBoard1';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
-
-// crear los objetos con los valores que tendrán las piezas del juego (esta data luego se guardara en firebase)
-let guidePiece = {
-  name: '',
-  img:'',
-  size: '',
-  orientation: ''
-};
-let piece1 = {
-  name: '1 cuadrado',
-  img:'',
-  sizeHorizontal:1,
-  sizeVertical:1,
-  orientation: 'vertical',
-  isPlaced:false 
-};
-let piece2 = {
-  name: '1 cuadrado',
-  img:'',
-  sizeHorizontal:1,
-  sizeVertical:1,
-  orientation: 'vertical',
-  isPlaced:false 
-};
-let piece3 = {
-  name: '2 cuadrados',
-  img:'',
-  sizeHorizontal:1,
-  sizeVertical:2,
-  orientation: 'vertical',
-  isPlaced:false 
-};
-let piece4 = {
-  name: '2 cuadrados',
-  img:'',
-  sizeHorizontal:1,
-  sizeVertical:2,
-  orientation: 'vertical',
-  isPlaced:false 
-};
-let piece5 = {
-  name: '3 cuadrados',
-  img:'',
-  sizeHorizontal:1,
-  sizeVertical:3,
-  orientation: 'vertical',
-  isPlaced:false 
-};
-let piece6 = {
-  name: '4 cuadrados',
-  img:'',
-  sizeHorizontal:2,
-  sizeVertical:2,
-  orientation: 'vertical',
-  isPlaced:false
-};
-let piece7 = {
-  name: '4 cuadrados',
-  img:'',
-  sizeHorizontal:2,
-  sizeVertical:2,
-  orientation: 'vertical',
-  isPlaced:false
-};
 
 // asignar valores a las piezas
-
-const selectPiece = piece => {
-  guidePiece = piece;
-  console.log(guidePiece);
-};
-
-const Pieces = () => {
-  const {selectedPiece,setSelectedPiece} = useContext(GameBoardPaintContext);
-
-  
+const ShowPieces = () => {
+  const {selectedPiece,setSelectedPiece,pieces} = useContext(GameBoardPaintContext);
   return (
     <div>
     <p>esta seleccionado la pieza: {selectedPiece.name}</p>
     <p>orientacion:{selectedPiece.orientation}</p>
     <p>tamaño:{selectedPiece.sizeHorizontal}x{selectedPiece.sizeVertical}</p>
-    
-    <Button disabled={piece1.isPlaced} onClick={() => {
-          setSelectedPiece(piece1);   
-          piece1.isPlaced=true;     
+    <ButtonGroup
+              variant="text"
+              color="primary"
+              aria-label="full-width contained primary button group"
+            >
+       <Button disabled={pieces.dog1.isPlaced} onClick={() => {
+          setSelectedPiece(pieces.dog1);   
         }}>perro1</Button>
       
-      <Button disabled={piece2.isPlaced}  onClick={() => {
-          setSelectedPiece(piece2); 
-          piece2.isPlaced=true;        
+        <Button disabled={pieces.dog2.isPlaced} onClick={() => {
+          setSelectedPiece(pieces.dog2);   
         }}>perro2</Button>
 
-      <Button disabled={piece3.isPlaced}  onClick={() => {
-          setSelectedPiece(piece3); 
-          piece3.isPlaced=true;        
+        <Button disabled={pieces.dog3.isPlaced} onClick={() => {
+          setSelectedPiece(pieces.dog3);   
         }}>perro3</Button>
 
-      <Button disabled={piece4.isPlaced}  onClick={() => {
-          setSelectedPiece(piece4); 
-          piece4.isPlaced=true;        
-        }}>perro4 </Button>
+        <Button disabled={pieces.dog4.isPlaced} onClick={() => {
+          setSelectedPiece(pieces.dog4);   
+        }}>perro4</Button>
 
-       <Button disabled={piece5.isPlaced}  onClick={() => {
-          setSelectedPiece(piece5); 
-          piece5.isPlaced=true;       
-        }}>perro5 </Button>
+        <Button disabled={pieces.dog5.isPlaced} onClick={() => {
+          setSelectedPiece(pieces.dog5);   
+        }}>perro5</Button>
         
-        <Button disabled={piece6.isPlaced}  onClick={() => {
-          setSelectedPiece(piece6);
-          piece6.isPlaced=true;         
-        }}>perro6 </Button>
+        <Button disabled={pieces.dog6.isPlaced} onClick={() => {
+          setSelectedPiece(pieces.dog6);   
+        }}>perro6</Button>
 
-        <Button disabled={piece7.isPlaced}  onClick={() => {
-          setSelectedPiece(piece7); 
-          piece7.isPlaced=true;        
-        }}>perro7 </Button>
-        
+        <Button disabled={pieces.dog7.isPlaced} onClick={() => {
+          setSelectedPiece(pieces.dog7);   
+        }}>perro7</Button>
+      </ButtonGroup> 
     </div>
   );
 };
 
-export default Pieces;
+export default ShowPieces;
