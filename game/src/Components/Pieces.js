bd5e993fd85d0c25f53811a0336f31bf393b214a
Zotapianola <zotapianola@gmail.com>
2019-12-12 15:31:23 -0400

    numeros en perros

--- game/src/Components/Pieces.js
+++ game/src/Components/Pieces.js
@@ -11,31 +11,31 @@ const ShowPieces = () => {
     <section id="dog-btns">
        <Button disabled={pieces.dog1.isPlaced} onClick={() => {
           setSelectedPiece(pieces.dog1);   
-        }}><i class="fas fa-dog"></i></Button>
+        }}><i class="fas fa-dog"> 1 </i></Button>
       
         <Button disabled={pieces.dog2.isPlaced} onClick={() => {
           setSelectedPiece(pieces.dog2);   
-        }}><i class="fas fa-dog"></i></Button>
+        }}><i class="fas fa-dog"> 1 </i></Button>
 
         <Button disabled={pieces.dog3.isPlaced} onClick={() => {
           setSelectedPiece(pieces.dog3);   
-        }}><i class="fas fa-dog"></i></Button>
+        }}><i class="fas fa-dog"></i> 2 </Button>
 
         <Button disabled={pieces.dog4.isPlaced} onClick={() => {
           setSelectedPiece(pieces.dog4);   
-        }}><i class="fas fa-dog"></i></Button>
+        }}><i class="fas fa-dog"></i> 2 </Button>
 
         <Button disabled={pieces.dog5.isPlaced} onClick={() => {
           setSelectedPiece(pieces.dog5);   
-        }}><i class="fas fa-dog"></i></Button>
+        }}><i class="fas fa-dog"></i> 3 </Button>
         
         <Button disabled={pieces.dog6.isPlaced} onClick={() => {
           setSelectedPiece(pieces.dog6);   
-        }}><i class="fas fa-dog"></i></Button>
+        }}><i class="fas fa-dog"></i> 4 </Button>
 
         <Button disabled={pieces.dog7.isPlaced} onClick={() => {
           setSelectedPiece(pieces.dog7);   
-        }}><i class="fas fa-dog"></i></Button>
+        }}><i class="fas fa-dog"></i> 4 </Button>
     </section>
   );
 };
