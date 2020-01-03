362e4d060d504f34757baf55ff50905dec1b6751
anabelcarrion <anabelcarrion3110@gmail.com>
2020-01-02 22:50:42 -0300

    Updates

---
+++ precache-manifest.7434a15585f11aa374e0a2088eb1a57b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51796483b55bae9e4f65a44ca171306c",
+    "url": "/SCL010-Battleship/index.html"
+  },
+  {
+    "revision": "e415e16af8b54a62c59d",
+    "url": "/SCL010-Battleship/static/css/main.6ba85ccb.chunk.css"
+  },
+  {
+    "revision": "fe4f3d03fce6353ea338",
+    "url": "/SCL010-Battleship/static/js/2.5a26710d.chunk.js"
+  },
+  {
+    "revision": "e415e16af8b54a62c59d",
+    "url": "/SCL010-Battleship/static/js/main.c9df58c0.chunk.js"
+  },
+  {
+    "revision": "e0b7d89ad4d4cd0036f1",
+    "url": "/SCL010-Battleship/static/js/runtime-main.f02e94b7.js"
+  }
+]);
